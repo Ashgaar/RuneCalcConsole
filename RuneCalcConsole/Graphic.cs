97dbf7113f3d2d17bba0d3eb057808c39b2da2d4
Mikael Olsson <zangiloxd@hotmail.com>
2023-11-08 12:19:40 +0100

    Add project files.

---
+++ RuneCalcConsole/Graphic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuneCalcConsole
+{
+    public class Graphic
+    {
+        public void Introduction() {
+            Console.WriteLine("/////////////////////////////////");
+            Console.WriteLine("/                               /");
+            Console.WriteLine("/      Welcome to runecalc      /");
+            Console.WriteLine("/                               /");
+            Console.WriteLine("/////////////////////////////////");
+        }
+
+        public void MainMenu()
+        {
+            Console.WriteLine("1. Show XP table");
+        }
+
+        public void XPTable()
+        {
+            Console.WriteLine("Lvl | XP | Diff");
+            Console.WriteLine("1\t0\t0");
+            Console.WriteLine("2\t83\t83");
+            Console.WriteLine("3\t174\t91");
+            Console.WriteLine("1 0 0");
+            Console.WriteLine("1 0 0");
+            Console.WriteLine("1 0 0");
+            Console.WriteLine("1 0 0");
+            Console.WriteLine("1 0 0");
+        }
+    }
+}
---
+++ RuneCalcConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RuneCalcConsole
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool menu = true;
+            Graphic graphic = new Graphic();
+
+            graphic.Introduction();
+
+            while (menu)
+            {
+                graphic.MainMenu();
+
+
+
+
+
+                Console.Write("Input: ");
+                string choice = Console.ReadLine();
+
+            }
+        }
+    }
+}
